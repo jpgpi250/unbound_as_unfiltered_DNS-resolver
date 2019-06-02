c5170932cb4f64bdd532f19f24323f1d9ea57bd2
jpgpi250 <jpgpi250@gmail.com>
2019-06-02 05:15:37 +0200

    Create compile_unbound.sh

---
+++ home/pi/compile_unbound.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get -y install libssl-dev
+sudo apt-get -y install libexpat1-dev
+
+sudo groupadd -g 991 unbound
+sudo useradd -c "unbound-1.9.1" -d /var/lib/unbound -u 991 -g unbound -s /bin/false unbound
+
+file=unbound-1.9.1
+mkdir -p unbound
+cd unbound
+wget https://nlnetlabs.nl/downloads/unbound/$file.tar.gz
+tar xzf $file.tar.gz 
+cd $file
+
+sudo ./configure --prefix=/usr --sysconfdir=/etc --disable-static --with-pidfile=/run/unbound.pid
+sudo make
+sudo make install
+cd ..
+cd ..
