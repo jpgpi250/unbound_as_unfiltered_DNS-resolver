17c717fca73b6b3bafa4cb4320dd2035a9adf742
jpgpi250 <jpgpi250@gmail.com>
2019-09-05 09:19:05 +0200

    Update compile_unbound.sh

--- home/pi/compile_unbound.sh
+++ home/pi/compile_unbound.sh
@@ -4,9 +4,9 @@ sudo apt-get -y install libssl-dev
 sudo apt-get -y install libexpat1-dev
 
 sudo groupadd -g 991 unbound
-sudo useradd -c "unbound-1.9.1" -d /var/lib/unbound -u 991 -g unbound -s /bin/false unbound
+sudo useradd -c "unbound-1.9.3" -d /var/lib/unbound -u 991 -g unbound -s /bin/false unbound
 
-file=unbound-1.9.1
+file=unbound-1.9.3
 mkdir -p unbound
 cd unbound
 wget https://nlnetlabs.nl/downloads/unbound/$file.tar.gz
