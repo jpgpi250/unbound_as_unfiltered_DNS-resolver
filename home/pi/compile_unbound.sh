1ef7086b5b33dd53449f19e0f8bf4ca8048db239
jpgpi250 <jpgpi250@gmail.com>
2019-10-17 15:54:58 +0200

    Update compile_unbound.sh

--- home/pi/compile_unbound.sh
+++ home/pi/compile_unbound.sh
@@ -3,17 +3,32 @@
 sudo apt-get -y install libssl-dev
 sudo apt-get -y install libexpat1-dev
 
-sudo groupadd -g 991 unbound
-sudo useradd -c "unbound-1.9.3" -d /var/lib/unbound -u 991 -g unbound -s /bin/false unbound
+# redis
+sudo apt-get -y install redis-server
+# reference configuration file changes https://habilisbest.com/install-redis-on-your-raspberrypi
+file=/etc/redis/redis.conf
+sudo sed -i '/# maxmemory <bytes>/a maxmemory 50M' $file
+sudo sed -i '/stop-writes-on-bgsave-error yes/s/^/#/g' $file
+sudo sed -i '/#stop-writes-on-bgsave-error yes/a stop-writes-on-bgsave-error no' $file
+sudo sed -i '/# maxmemory-policy noeviction/a maxmemory-policy allkeys-lru' $file
+sudo service redis stop
+sudo service redis start
 
-file=unbound-1.9.3
+# library, required to compile unbound --with-libhiredis
+sudo apt-get install libhiredis-dev
+
+# user
+sudo groupadd -g 991 unbound
+sudo useradd -c "unbound-1.9.4" -d /var/lib/unbound -u 991 -g unbound -s /bin/false unbound
+
+file=unbound-1.9.4
 mkdir -p unbound
 cd unbound
 wget https://nlnetlabs.nl/downloads/unbound/$file.tar.gz
 tar xzf $file.tar.gz 
 cd $file
 
-sudo ./configure --prefix=/usr --sysconfdir=/etc --disable-static --with-pidfile=/run/unbound.pid
+sudo ./configure --prefix=/usr --sysconfdir=/etc --disable-static --with-libhiredis --with-pidfile=/run/unbound.pid
 sudo make
 sudo make install
 cd ..
