67f6a7b8df447e17c49d565926fdc80f61695113
jpgpi250 <jpgpi250@gmail.com>
2019-10-30 09:39:54 +0100

    Update compile_unbound.sh

--- home/pi/compile_unbound.sh
+++ home/pi/compile_unbound.sh
@@ -8,10 +8,21 @@ sudo apt-get -y install libevent-dev
 sudo apt-get -y install redis-server
 # reference configuration file changes https://habilisbest.com/install-redis-on-your-raspberrypi
 file=/etc/redis/redis.conf
-sudo sed -i '/# maxmemory <bytes>/a maxmemory 50M' $file
+sudo sed -i '/tcp-backlog 511/s/^/#/g' $file
+sudo sed -i '/#tcp-backlog 511/a tcp-backlog 63' $file
+sudo sed -i '/syslog-enabled no/s/^# //g' $file
+sudo sed -i '/databases 16/s/^/#/g' $file
+sudo sed -i '/#databases 16/a databases 1' $file
+sudo sed -i '/always-show-logo yes/s/^/#/g' $file
+sudo sed -i '/#always-show-logo yes/a always-show-logo no' $file
 sudo sed -i '/stop-writes-on-bgsave-error yes/s/^/#/g' $file
 sudo sed -i '/#stop-writes-on-bgsave-error yes/a stop-writes-on-bgsave-error no' $file
+sudo sed -i '/rdbcompression yes/s/^/#/g' $file
+sudo sed -i '/#rdbcompression yes/a rdbcompression no' $file
+sudo sed -i '/# maxmemory <bytes>/a maxmemory 32M' $file
 sudo sed -i '/# maxmemory-policy noeviction/a maxmemory-policy allkeys-lru' $file
+sudo sed -i '/slowlog-max-len 128/s/^/#/g' $file
+sudo sed -i '/#slowlog-max-len 128/a slowlog-max-len 16' $file
 sudo service redis stop
 sudo service redis start
 
